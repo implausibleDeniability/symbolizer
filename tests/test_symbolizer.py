38c8e10540c94fb5f71321212d52507f95411e67
Maxim Faleev <m.faleev@innopolis.university>
2023-11-18 11:08:59 +0400

    + 4 symbolizer tests

--- tests/test_symbolizer.py
+++ tests/test_symbolizer.py
@@ -8,6 +8,7 @@ def set_seed():
 
 
 def test_sum_of_two_variables():
+    set_seed()
     x = np.random.randn(3, 2)
     y = x[:, 0] + x[:, 1]
     
@@ -16,3 +17,46 @@ def test_sum_of_two_variables():
 
     assert expression == 'x0 + x1'
 
+
+def test_product_of_two_variables():
+    set_seed()
+    x = np.random.randn(3, 2)
+    y = x[:, 0] * x[:, 1]
+    
+    symbolizer = Symbolizer(x, y)
+    expression = symbolizer.run()
+
+    assert expression == '(x0) * (x1)'
+
+
+def test_division():
+    set_seed()
+    x = np.random.randn(3, 2)
+    y = x[:, 0] / x[:, 1]
+    
+    symbolizer = Symbolizer(x, y)
+    expression = symbolizer.run()
+
+    assert expression == '(x0) / (x1)'
+
+
+def test_three_variables():
+    set_seed()
+    x = np.random.randn(5, 3)
+    y = (x[:, 0] + x[:, 1]) * x[:, 2]
+
+    symbolizer = Symbolizer(x, y, max_complexity=5)
+    expression = symbolizer.run()
+
+    assert expression == '(x2) * (x0 + x1)'
+
+
+def test_four_variables():
+    set_seed()
+    x = np.random.randn(5, 4)
+    y = (x[:, 0] + x[:, 1] * x[:, 2]) / (x[:, 0] + x[:, 3])
+
+    symbolizer = Symbolizer(x, y, max_complexity=10)
+    expression = symbolizer.run()
+
+    assert expression == '(x0 + (x1) * (x2)) / (x0 + x3)'
